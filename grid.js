e05725589f3a3145d0387bf97eb6442455a2f2bf
bill-laughlin <123513007+bill-laughlin@users.noreply.github.com>
2023-04-15 20:38:09 -0500

    Updated description

--- grid.js
+++ grid.js
@@ -475,7 +475,7 @@ function generateBox(){
 function directions(){
     alert(`    Welcome to The A-Maze-ing Escape!\n
     To create a new maze before you finish, select 'Generate New Maze'.\n
-    Use the arrow keys to navigate and find the key.\n
+    Use the arrow keys or 'WASD' to navigate and find the key.\n
     Finally navigate to the exit door and ESCAPE.\n
     Adjust the 'Maze Size' and 'Visibility' as you desire.\n
     `)
